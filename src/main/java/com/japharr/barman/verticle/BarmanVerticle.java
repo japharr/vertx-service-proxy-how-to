e0aa6d9f207bf1440e494553647a3250a42c0734
Jelil Adesina <jtutoff@gmail.com>
2021-08-16 10:43:57 +0100

    done: success

--- src/main/java/com/japharr/barman/verticle/BarmanVerticle.java
+++ src/main/java/com/japharr/barman/verticle/BarmanVerticle.java
@@ -5,15 +5,42 @@ import com.japharr.barman.service.proxy.impl.BarmanServiceImpl;
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Promise;
 import io.vertx.ext.web.client.WebClient;
+import io.vertx.servicediscovery.Record;
+import io.vertx.servicediscovery.ServiceDiscovery;
+import io.vertx.servicediscovery.ServiceDiscoveryOptions;
+import io.vertx.servicediscovery.impl.DiscoveryImpl;
+import io.vertx.servicediscovery.types.EventBusService;
 import io.vertx.serviceproxy.ServiceBinder;
 
 public class BarmanVerticle extends AbstractVerticle {
+  private ServiceDiscovery discovery;
+  private Record publishedRecord;
+
   @Override
   public void start() throws Exception {
+    ServiceDiscovery discovery = new DiscoveryImpl(vertx,
+      new ServiceDiscoveryOptions());
+
     BarmanService service = new BarmanServiceImpl(WebClient.create(vertx));
 
     new ServiceBinder(vertx)
       .setAddress("beers.service.app")
       .register(BarmanService.class, service);
+
+    Record record = EventBusService.createRecord(
+      "barman-service",
+      "beers.service.app",
+      BarmanService.class
+    );
+
+    discovery.publish(record, ar -> {
+      if (ar.succeeded()) {
+        // publication success
+        publishedRecord = ar.result();
+      } else {
+        // publication failure
+        ar.cause().printStackTrace();
+      }
+    });
   }
 }
--- src/main/java/com/japharr/barman/verticle/DrunkVerticle.java
+++ src/main/java/com/japharr/barman/verticle/DrunkVerticle.java
@@ -3,12 +3,27 @@ package com.japharr.barman.verticle;
 import com.japharr.barman.service.proxy.BarmanService;
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Promise;
+import io.vertx.core.json.JsonObject;
+import io.vertx.servicediscovery.ServiceDiscovery;
+import io.vertx.servicediscovery.ServiceReference;
 
 public class DrunkVerticle extends AbstractVerticle {
   @Override
   public void start(Promise<Void> startPromise) {
-    BarmanService service = BarmanService.createProxy(vertx, "beers.service.app");
+    ServiceDiscovery discovery = ServiceDiscovery.create(vertx);
+    discovery.getRecord(new JsonObject().put("name", "barman-service"), ar -> {
+      if (ar.succeeded() && ar.result() != null) {
+        ServiceReference reference = discovery.getReference(ar.result());
+        BarmanService service = reference.getAs(BarmanService.class);
 
+        performOperation(service, startPromise);
+
+        reference.release();
+      }
+    });
+  }
+
+  private void performOperation(BarmanService service, Promise<Void> startPromise) {
     service.giveMeARandomBeer("homer", b1 -> {
       if (b1.failed()) {
         System.err.println("Cannot get my first beer!");
