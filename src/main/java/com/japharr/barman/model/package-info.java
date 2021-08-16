5ea02bd52ee45aa39c885cbca25f6f2a2b47040c
Jelil Adesina <jtutoff@gmail.com>
2021-08-16 03:09:57 +0100

    added gitignore

--- src/main/java/com/japharr/barman/model/package-info.java
+++
@@ -1,4 +0,0 @@
-@ModuleGen(groupPackage = "com.japharr.barman", name = "model")
-package com.japharr.barman.model;
-
-import io.vertx.codegen.annotations.ModuleGen;
--- src/main/java/com/japharr/barman/service/proxy/package-info.java
+++
@@ -1,4 +0,0 @@
-@ModuleGen(groupPackage = "com.japharr.barman.service", name = "proxy")
-package com.japharr.barman.service.proxy;
-
-import io.vertx.codegen.annotations.ModuleGen;
